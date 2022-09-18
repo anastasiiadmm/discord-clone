6c2422770c73ee216ea443e1ed52941957a7ccad
anastasiiadmm <37694421+anastasiiadmm@users.noreply.github.com>
2022-09-18 16:22:45 +0600

    Realized side bar and icons

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,127 @@
 </script>
 
 <template>
+  <div class="flex">
+    <div class="bg-gray-900 w-20 flex-none flex flex-col min-h-screen h-screen px-2 py-2">
+      <div class="overflow-y-auto">
+        <ul class="text-center">
+          <li>
+            <a href="">
+              <img src="./assets/images/discord-logo.png"
+                   alt="discord"
+                   class="w-12 h-12 rounded-full mx-auto"
+              />
+            </a>
+          </li>
+          <li class="border-b border-gray-700 mx-4 mt-3"></li>
+          <li class="mt-3">
+            <a href="#">
+              <img src="./assets/images/vue-js-icon.png" alt="vue"
+                   class="w-9 h-7 rounded-full mx-auto"
+              />
+            </a>
+          </li>
+          <li class="mt-3">
+            <a href="#" class="hover:bg-teal-500 hover:text-white">
+              <img src="./assets/images/tailwind-css-icon.svg" alt="tailwind"
+                   class="w-11 h-10 rounded-full mx-auto"
+              />
+            </a>
+          </li>
+          <li class="mt-3">
+            <a href="#" class="hover:bg-teal-500 hover:text-white w-12 h-12 bg-gray-800 text-teal-500 inline-block rounded-full">
+              <img class="w-5 h-5 mx-auto my-3.5" src="./assets/images/plus-sign.png" />
+            </a>
+          </li>
+          <li class="border-b border-gray-700 mx-4 mt-3"></li>
+          <li class="mt-3">
+            <a href="#" class="hover:bg-teal-500 hover:text-white w-12 h-12 bg-gray-800 text-teal-500 inline-block rounded-full">
+              <img class="w-5 h-5 mx-auto my-3.5" src="./assets/images/compass-svgrepo-com.svg" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="flex flex-col min-h-screen h-screen">
+      <div class="bg-red-500 flex">
+        <div class="bg-red-300 w-56 flex-none">Css</div>
+        <div>Rest of top bar</div>
+      </div>
+      <div class="flex-1 bg-blue-500 flex overflow-y-hidden">
+        <div class="bg-green-300 w-56 flex-none flex flex-col justify-between">
+          <div class="overflow-y-auto">
+            In nisl metus, consectetur in sollicitudin eu, semper vel elit. Nunc nunc erat, ornare at ullamcorper ac,
+            pulvinar at sapien. Sed nec feugiat lorem, eu porta ipsum. Proin finibus neque mauris, sit amet convallis
+            leo
+            elementum quis. Vestibulum vitae pellentesque arcu, a blandit tellus. Aliquam consequat blandit felis, quis
+            ullamcorper est sagittis nec. Sed tristique a tortor blandit viverra. In eu nulla eget lectus eleifend
+            aliquet
+            Integer condimentum at massa in vulputate. Proin nec odio elementum, placerat mauris vitae, dignissim diam.
+            Donec ullamcorper imperdiet fermentum. Integer sagittis, lectus tempor pretium semper, elit augue consequat
+            quam, blandit elementum erat libero id diam. In sodales tincidunt nunc, eu iaculis turpis suscipit a. In
+            porta
+            neque eleifend eleifend congue. Ut ac hendrerit mauris, efficitur lobortis massa. Duis tristique ligula eget
+            arcu venenatis dignissim. Aliquam euismod nisi vitae vestibulum facilisis. Fusce sagittis metus nec justo
+            interdum suscipit quis ut felis. Nullam sed imperdiet erat. Cras sed leo efficitur, vehicula ipsum at,
+            euismod
+            libero. Nulla eleifend facilisis malesuada. Nunc vehicula ac elit sit amet sollicitudin. Quisque feugiat
+            justo
+            at erat venenatis, in efficitur massa feugiat.
+          </div>
+          <div class="bg-gray-500 bottom">
+            Bottom
+          </div>
+        </div>
+        <div class="bg-green-500 flex-1 flex justify-between">
+          <div class="bg-indigo-300 flex-1 flex flex-col justify-between">
+            <div class="overflow-y-auto">
+              Display. Integer condimentum at massa in vulputate. Proin nec odio elementum, placerat mauris vitae,
+              dignissim diam. Donec ullamcorper imperdiet fermentum. Integer sagittis, lectus tempor pretium semper,
+              elit
+              augue consequat quam, blandit elementum erat libero id diam. In sodales tincidunt nunc, eu iaculis turpis
+              suscipit a. In porta neque eleifend eleifend congue. Ut ac hendrerit mauris, efficitur lobortis massa.
+              Duis
+              tristique ligula eget arcu venenatis dignissim. Aliquam euismod nisi vitae vestibulum facilisis. Fusce
+              sagittis metus nec justo interdum suscipit quis ut felis. Nullam sed imperdiet erat. Cras sed leo
+              efficitur,
+              vehicula ipsum at, euismod libero. Nulla eleifend facilisis malesuada. Nunc vehicula ac elit sit amet
+              sollicitudin. Quisque feugiat justo at erat venenatis, in efficitur massa feugiat.
+              In nisl metus, consectetur in sollicitudin eu, semper vel elit. Nunc nunc erat, ornare at ullamcorper ac,
+              pulvinar at sapien. Sed nec feugiat lorem, eu porta ipsum. Proin finibus neque mauris, sit amet convallis
+              leo elementum quis. Vestibulum vitae pellentesque arcu, a blandit tellus. Aliquam consequat blandit felis,
+              quis ullamcorper est sagittis nec. Sed tristique a tortor blandit viverra. In eu nulla eget lectus
+              eleifend
+              aliquet.
+            </div>
+            <div class="bg-gray-500 bottom">
+              Bottom
+            </div>
+          </div>
 
+          <div class="bg-indigo-600 w-56 flex-none overflow-y-auto">Users. Integer condimentum at massa in vulputate.
+            Proin nec odio elementum, placerat mauris vitae, dignissim diam. Donec ullamcorper imperdiet fermentum.
+            Integer sagittis, lectus tempor pretium semper, elit augue consequat quam, blandit elementum erat libero id
+            diam. In sodales tincidunt nunc, eu iaculis turpis suscipit a. In porta neque eleifend eleifend congue. Ut
+            ac
+            hendrerit mauris, efficitur lobortis massa. Duis tristique ligula eget arcu venenatis dignissim. Aliquam
+            euismod nisi vitae vestibulum facilisis. Fusce sagittis metus nec justo interdum suscipit quis ut felis.
+            Nullam sed imperdiet erat. Cras sed leo efficitur, vehicula ipsum at, euismod libero. Nulla eleifend
+            facilisis
+            malesuada. Nunc vehicula ac elit sit amet sollicitudin. Quisque feugiat justo at erat venenatis, in
+            efficitur
+            massa feugiat.
+
+            In nisl metus, consectetur in sollicitudin eu, semper vel elit. Nunc nunc erat, ornare at ullamcorper ac,
+            pulvinar at sapien. Sed nec feugiat lorem, eu porta ipsum. Proin finibus neque mauris, sit amet convallis
+            leo
+            elementum quis. Vestibulum vitae pellentesque arcu, a blandit tellus. Aliquam consequat blandit felis, quis
+            ullamcorper est sagittis nec. Sed tristique a tortor blandit viverra. In eu nulla eget lectus eleifend
+            aliquet.
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
